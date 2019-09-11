8e3db0bd483a86d8dc578f5532dc0b9a1c37e3dc
Charl Alexander Baello <52141562+charlbaello@users.noreply.github.com>
2019-09-12 07:10:50 +0800

    Update R6.php

--- R6.php
+++ R6.php
@@ -31,7 +31,7 @@
 </nav>
 <!--WELCOME-->
 <div class="container-fluid padding">
-<div class="row welcome text-center">
+<div class="row welcome text-left">
 <hr>
 <p class="lead"> </br><h1>Reflection 6</h1></br></br></p>
 <p>
