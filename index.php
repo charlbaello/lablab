1ea37456ef7d3f77e52e89e5340f3b727262bea8
Charl Alexander Baello <52141562+charlbaello@users.noreply.github.com>
2019-09-12 08:20:13 +0800

    Update index.php

--- index.php
+++ index.php
@@ -55,12 +55,6 @@
 			<a href="Reflection.php" type="button" class="btn btn-outline-light btn-lg">REFLECTIONS</a>
 		</div>
 	</div>
-	<div class="carousel-item">
-		<img src="https://images.wallpaperscraft.com/image/lights_light_lighting_117051_1920x1080.jpg">
-		<div class="carousel-caption">
-			<h2 class="display-2">Certification</h2>
-			<h4>Page</h4>
-			<a href="Certification.php" type="button" class="btn btn-outline-light btn-lg">CERTIFICATION</a>
 </div>
 </div>
 
