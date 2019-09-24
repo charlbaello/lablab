4908ae8b9c9ae9a5f9db19563f8f6769f8ac4a5e
Charl Alexander Baello <52141562+charlbaello@users.noreply.github.com>
2019-09-24 15:21:09 +0800

    Update Certification.php

--- Certification.php
+++ Certification.php
@@ -48,6 +48,16 @@
 <p></br><h2>Invision for UX design</h2></br>
 <p style="text-align:center;">September 16,2019</p>
 <img src="https://i.imgur.com/fMrx68K.png">
+<p></br><h2>Python Basic for Java Developers</h2></br>
+<p style="text-align:center;">January 30,2019</p>
+<img src="blob:https://imgur.com/8b766268-3496-478c-a064-a446a7dbd11a">
+<p></br><h2>Managing Your Time</h2></br>
+<p style="text-align:center;">January 30,2019</p>
+<img src="https://i.imgur.com/Cyyo4Db.png">
+<p></br><h2>Body Language For Leaders(2015)</h2></br>
+<p style="text-align:center;">June 10,2018</p>
+<img src="https://i.imgur.com/gVW4Rak.png">
+
 
 </p>
 	</div>
