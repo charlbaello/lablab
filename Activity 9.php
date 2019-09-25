378841f3e2a0736020016c45e31d1f8fe70e81fe
Charl Alexander Baello <52141562+charlbaello@users.noreply.github.com>
2019-09-25 07:12:38 -0700

    Add files via upload

---
+++ Activity 9.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="with=device-with, initial-scale=1">
+	<title>USER DES UI UX</title>
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+	<script src="https://use.fontawesome.com/releases/v5.0.8/js/all.js"></script>
+	<link href="style.css" rel=stylesheet>
+</head>
+<!-- Navigation Bar-->
+<!-- Navigation Bar-->
+<nav class="navbar navbar-expand-md navbar-light bg-light sticky-top">
+<div class="container-fluid">
+	<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive">
+			<span class="navbar-toggler-icon"></span>
+	</button>
+	<div class="collapse navbar-collapse" id="navbarResponsive">
+		<ul class="navbar-nav ml-auto">
+			<li class="nav-item active">
+				<a class="nav-link" href="index.php">Home</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="About me.php">About</a>
+			</li>
+			<li class="nav-item active">
+				<a class="nav-link" href="Certification.php">Certification</a>
+			</li>
+	</div>
+</div>
+</nav>
+<body background="https://i.imgur.com/FdRxV9d.jpg">
+<!--WELCOME-->
+<div class="container-fluid padding">
+<div class="row welcome text-center">
+	<hr>
+	<div class="col-12">
+            <p class="lead"> <h1>Activity 9</h1></p>
+				
+            <h2>12 Column Grid Layout Design</h2>
+        
+        <p><center>Below you can see my personalized 12 grid column layout design.</center></p>
+            
+            
+            <script src="https://ajax.googleapis.com/ajax/libs/jquery/2.1.1/jquery.min.js"></script>
+            <!-- Trigger the Modal -->
+            <!-- 4x4 layout grid -->
+            <center>
+            <img class="myImg" src="https://i.imgur.com/liEDh34.png" alt="12x4 layout 1" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/hEGh844.png" alt="12x4 layout 2" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/JVW1Zz3.png" alt="12x4 layout 3" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/ibdQRaz.png" alt="12x4 layout 4" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <pre>
+            12x4 layout 1				12x4 layout 2				12x4 layout 3				12x4 layout 4
+            </pre>
+            <img class="myImg" src="https://i.imgur.com/KqjtLAI.png" alt="12x4 layout 5" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/qdXKlKd.png" alt="12x4 layout 6" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/oZoA9D6.png" alt="12x4 layout 7" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/nTpPnN8.png" alt="12x4 layout 8" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <pre>
+            12x4 layout 5				12x4 layout 6				12x4 layout 7				12x4 layout 8
+            </pre>
+            <img class="myImg" src="https://i.imgur.com/Jc8uCcp.png" alt="12x4 layout 9" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/qQd2tMI.png" alt="12x4 layout 10" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/QAe0jff.png" alt="12x4 layout 11" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <img class="myImg" src="https://i.imgur.com/tzGoJ1s.png" alt="12x4 layout 12" style="width:100%;max-width:300px;padding-bottom: 10px">
+            <pre>
+            12x4 layout 9				12x4 layout 10				12x4 layout 11				12x4 layout 12
+            </pre>
+            </center>
+            </p>
+	</div>
+</div>
+</div>
+<li><a href="Activities.php"><h3>GO BACK</h3></a></li
